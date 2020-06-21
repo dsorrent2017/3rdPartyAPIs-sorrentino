31b84b271b6acb508b24da4f0610ee1078a2533f
Owner <dsorrent7@gmail.com>
2020-06-21 05:07:02 -0400

    Add programming concepts covered in class to date like associating highly cohesive code into functions.  Also, add comments to html and bootstrap - css code as this is my greatest need for improvement in my career.

--- script.js
+++ script.js
@@ -12,89 +12,125 @@ See index.html for
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 
 */
+
+
+//global variables
+const momentTime = moment().format('MMMM Do YYYY');
+let militaryHours = moment().format('H');
+const debug = true;
+//Use font-awesome as imported in index.html
+const saveImageButton = "./images/save-regular.svg"; 
+
+
+//loop control variables : Future enhancement: make configureable with input text boxes
+const numberHourRows = 18;
+let hourDescriptions =  new Array(numberHourRows);  //ES6 like Java && C# makes  Array a 1st class object
+const beginHour = 6;
+const endHour = 24;
+
+//compute at runtime so set to null;
+let displayHour = null;
+let amOrPm =  null;
+let $saveBtn = null;
+
+//global functions
+function updateCurrentTimeWidget(){
+  const $currentDay = $('#currentDay');
+  $currentDay.text(momentTime);
+}
+
+function getHourlyPlans(){
+  return JSON.parse(localStorage.getItem("hourlyPlans"));  //convert localStorage string into array of JSON String-objects
+}
+
+function setupHoursCell(){
+  let $hourColDiv = $('<div>');
+  $hourColDiv.addClass('col-md-2');
+  const $hourSpanCell = $('<span>');  //Spans are great for styling table cells
+  
+  return $hourColDiv;
+}
+
+function setupHourSpanCell(hour){
+  const $hourSpanCell = $('<span>');
+  $hourSpanCell.attr('class','hourSpan');
+  displayHour = hour > 12 ? hour - 12 : hour;
+  amOrPm =  hour > 12 ? "PM": "AM";
+  $hourSpanCell.text(`${displayHour} ${amOrPm}`);
+  return $hourSpanCell;
+}
+
+function setupDescriptionCell(index) {
+  let $descriptionText = $("<input>");
+
+  $descriptionText.attr("id", `input-${index}`);
+  $descriptionText.attr("hour-index", index);
+  $descriptionText.attr("type", "text");
+  $descriptionText.attr("class", "dailyPlan");
+
+  $descriptionText.val(hourDescriptions[index]); //get the saved description
+
+  return $descriptionText;
+}
+
+function setupSaveButtonCell(index){
+  let $saveButtonCell =  $('<div>');
+  $saveButtonCell.addClass('col-md-1');
+  //function with side effect of populating global variable -- not always best practice
+  $saveBtn = $('<i>');
+  $saveBtn.attr('id',`saveid-${index}`);
+  $saveBtn.attr('save-id',index);
+  $saveBtn.attr('class',"far fa-save saveImageButton");
+
+  return $saveButtonCell;
+}
+      
+
+
 $(document).ready(function() {
     debugger;
-    const debug = true;
-    const momentTime = moment().format('MMMM Do YYYY');
-    let militaryHours = moment().format('H');
-    const $currentDay = $('#currentDay');
-    $currentDay.text(momentTime);
+    updateCurrentTimeWidget();
 
-    //Use font-awesome as covered in week 1
-    const saveImageButton = "./images/save-regular.svg"; 
-  
-    let hourlyPlans = JSON.parse(localStorage.getItem("hourlyPlans"));  //convert localStorage string into JSON Object
-    debugger;
-    const numberHourRows = 18;
+    let hourlyPlans = getHourlyPlans();
     if (hourlyPlans !== null) {
       hourDescriptions = hourlyPlans;
-    } else {
-      hourDescriptions = new Array(numberHourRows);  
-    }
+    } 
     
-    let $hourSchedulerDiv = $('#hourSchedulerContainer');
-    $hourSchedulerDiv.empty();
-  
-    const beginHour = 6;
-    const endHour = 24;
-  
-    //my day starts at 6 AM
+    let $hourSchedulerDiv = $('#hourSchedulerContainer');  //The bootstrap way of creating a table, see index.html
+    $hourSchedulerDiv.empty(); //clear table on Reload and render it in the loop below
+    
     for (let hour = beginHour; hour <= endHour; hour++) {
       let index = hour - beginHour ;   //0 based table index
      
-     
-     
-     //Build rows using JQuery Library methods: addClass(), attr()
+      //Build rows using JQuery Library methods: addClass(), attr()
+      
+      //A row div can be added to a 'container table' : use JQuuery convention of $ to begin variables initialized from JQuery library
       let $rowDiv = $('<div>');
-
-    
       $rowDiv.addClass('row');
       $rowDiv.addClass('hourSchedulerRow');
       $rowDiv.attr('hour-index',hour);
     
-      let $hourColDiv = $('<div>');
-      $hourColDiv.addClass('col-md-2');
-    
-
-      const $hourSpanCell = $('<span>');
-      $hourSpanCell.attr('class','hourSpan');
-      
-      let displayHour = hour > 12 ? hour - 12 : hour;
-      let amOrPm =  hour > 12 ? "PM": "AM";
-      
-      $hourSpanCell.text(`${displayHour} ${amOrPm}`);
-  
+      //set up table cell for hours column  as a DIV -- use as many concepts covered in class as possible
+      let $hourSpanCell = setupHourSpanCell(hour);
+      let $hourColDiv = setupHoursCell();
       $rowDiv.append($hourColDiv);
       $hourColDiv.append($hourSpanCell);
 
-      let $dailyPlanSpn = $('<input>');
-  
-      $dailyPlanSpn.attr('id',`input-${index}`);
-      $dailyPlanSpn.attr('hour-index',index);
-      $dailyPlanSpn.attr('type','text');
-      $dailyPlanSpn.attr('class','dailyPlan');
- 
-      $dailyPlanSpn.val( hourDescriptions[index] ); //get the saved description
+      let $descriptionText = setupDescriptionCell(index);
       
- 
       let $descriptionDiv = $('<div>');
       $descriptionDiv.addClass('col-md-9');
 
       $rowDiv.append($descriptionDiv);
-      $descriptionDiv.append($dailyPlanSpn);
+      $descriptionDiv.append($descriptionText);
 
-      let $col1SaveDiv = $('<div>');
-      $col1SaveDiv.addClass('col-md-1');
-  
-      let $saveBtn = $('<i>');
-      $saveBtn.attr('id',`saveid-${index}`);
-      $saveBtn.attr('save-id',index);
-      $saveBtn.attr('class',"far fa-save saveImageButton");
+      let $saveButtonCell = setupSaveButtonCell(index);
       
-      $rowDiv.append($col1SaveDiv);
-      $col1SaveDiv.append($saveBtn);
+      
+      $rowDiv.append($saveButtonCell);
+      $saveButtonCell.append($saveBtn);
   
-      changeRowColorOnCondition($rowDiv, hour);
+      changeRowColorOnCondition($rowDiv, hour);  //highlight all cells in row based on time
       
       $hourSchedulerDiv.append($rowDiv);
     };
@@ -104,13 +140,16 @@ $(document).ready(function() {
   */
     function changeRowColorOnCondition ($hourRow,hour) { 
       debugger;
+      let backgroundColor = null;
       if ( hour < militaryHours) {
-        $hourRow.css("background-color","lightgrey")
+        backgroundColor = "lightgrey";
       } else if ( hour > militaryHours) {
-        $hourRow.css("background-color","lightgreen")
+        backgroundColor = "lightgreen";
       } else {
-        $hourRow.css("background-color","tomato")
+        backgroundColor = "orange";
       }
+ 
+      $hourRow.children('td').css({ "background-color": backgroundColor });
 
     };
   
